7955fc161a9ca26d586a971a175eda49501fba1c
Sebastian Hösl <sebastian.hoesl@outlook.de>
2016-09-21 16:07:54 +0200

    Added Gulpfile for eps to svg conversation

---
+++ Gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var cloudConvertSettings = JSON.parse(fs.readFileSync('./cloudConvertSettings.json'));
+var cloudconvert = new(require('cloudconvert'))(cloudConvertSettings.apiKey);
+var remoteSrc = require('gulp-remote-src');
+
+gulp.task('default', function() {
+    console.log(cloudConvertSettings.apiKey);
+    fs.createReadStream('./src/img/de_badge_web_generic.eps')
+        .pipe(cloudconvert.convert({
+            inputformat: 'eps',
+            outputformat: 'svg'
+        }))
+        .pipe(fs.createWriteStream('de_badge_web_generic.svg'))
+        .on('finish', function() {
+            console.log('Done!');
+        });
+
+    /*
+        remoteSrc(['en_badge_web_generic.eps', 'de_badge_web_generic.eps'], {
+                base: 'https://play.google.com/intl/en_us/badges/images/generic/',
+                requestOptions: {
+                    'proxy': 'http://shsl:s987123H%26@alproxy1.my-tts.net:8080'
+                }
+            })
+            .pipe(cloudconvert.convert({
+                inputformat: 'eps',
+                outputformat: 'svg'
+            }))
+            .pipe(gulp.dest('./src/img/'));
+        /*
+
+        fs.createReadStream('tests/input.png')
+            .pipe(cloudconvert.convert({
+                inputformat: 'eps',
+                outputformat: 'svg'
+            }))
+            .pipe(fs.createWriteStream('out.jpg'))
+            .on('finish', function() {
+                console.log('Done!');
+            });*/
+});
